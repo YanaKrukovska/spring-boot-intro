f9f76bdc0bda7291f83e3882dcf399cf6bf96e6b
YanaKrukovska <jana.krua@gmail.com>
2021-11-07 23:21:09 +0200

    Attempt to add oauth2

--- src/main/java/com/krukovska/springbootintro/service/impl/UserServiceImpl.java
+++ src/main/java/com/krukovska/springbootintro/service/impl/UserServiceImpl.java
@@ -4,12 +4,17 @@ import com.krukovska.springbootintro.model.User;
 import com.krukovska.springbootintro.repository.UserRepository;
 import com.krukovska.springbootintro.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
-public class UserServiceImpl implements UserService {
+public class UserServiceImpl implements UserService, UserDetailsService {
 
     private final UserRepository repository;
 
@@ -34,4 +39,19 @@ public class UserServiceImpl implements UserService {
     }
 
 
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUsername(username);
+
+        if (user == null) {
+            throw new UsernameNotFoundException(username);
+        }
+
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                getAuthority());
+    }
+
+    private List<SimpleGrantedAuthority> getAuthority() {
+        return Collections.singletonList(new SimpleGrantedAuthority("ROLE_ADMIN"));
+    }
 }
